6f391aad3782fa5c2a3d1a27cb8539b37b847bfd
Rishika Maheshwari <2024tm93731@wilp.bits-pilani.ac.in>
2026-03-09 20:41:41 +0530

    Version 1.1 client management added

--- app/main.py
+++ app/main.py
@@ -1,26 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 programs = {
-    "Fat Loss": {
-        "workout": "Back Squat + Cardio",
-        "diet": "Egg Whites + Oats"
-    },
-    "Muscle Gain": {
-        "workout": "Squat + Bench + Deadlift",
-        "diet": "Eggs + Chicken"
-    },
-    "Beginner": {
-        "workout": "Pushups + Air Squats",
-        "diet": "Balanced Diet"
-    }
+    "Fat Loss": {"factor":22},
+    "Muscle Gain": {"factor":35},
+    "Beginner": {"factor":26}
 }
+
+clients = []
 
 @app.route("/")
 def home():
-    return "ACEest Fitness API Version 1.0"
+    return "ACEest Fitness API Version 1.1"
 
 @app.route("/programs")
 def get_programs():
     return jsonify(programs)
+
+@app.route("/clients", methods=["GET"])
+def get_clients():
+    return jsonify(clients)
+
+@app.route("/clients/add", methods=["POST"])
+def add_client():
+    data = request.json
+    clients.append(data)
+    return {"message":"client added"}
